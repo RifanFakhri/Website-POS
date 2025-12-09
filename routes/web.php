235afe8f500b28690d82cfdefab6d06b7467aa01
fakhririfan86 <fakhririfan86@gmail.com>
2025-12-09 09:24:00 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,9 @@ Route::get('/dashboard', function () {
 
 // GROUP ROUTE dengan PREFIX 'pages'
 Route::prefix('pages')->group(function () {
-    
-    // 1. Resource route untuk Produk (CRUD DETAIL PRODUK)
-    // URL Index: /pages/products
-    // Nama Index: products.index
+
     Route::resource('products', ProductController::class)->except(['create', 'show', 'edit']);
 
-    // 2. Route KHUSUS untuk Manajemen Stok (Lihat Stok & Tambah Stok)
-    // URL Index: /pages/stocks
-    // Nama Index: stocks.index
     Route::get('stocks', [ProductStockController::class, 'index'])->name('stocks.index');
     Route::post('stocks', [ProductStockController::class, 'store'])->name('stocks.store');
     
